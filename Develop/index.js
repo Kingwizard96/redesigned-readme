b196f75f19905ac8864126dd20195477ad1c6730
Kingwizard96 <calvarado1996@gmail.com>
2023-09-25 19:01:23 -0500

    made changes to markdown

--- Develop/index.js
+++ Develop/index.js
@@ -5,54 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [
         
-    // user info 
-    // name
-         {
+        // user info 
+        // name
+            {
+                type: 'input',
+                name: 'userInfo',
+                message:"what's your name?",
+                validate:userInfo => {
+                    if (userInfo) {
+                        return true;
+                    }else{
+                        console.log("please enter a valid name!");
+                        return false;
+                    }
+                }
+        
+            },
+    
+        // location
+        {
             type: 'input',
-            name: 'userInfo',
-            message:"what's your name?",
+            name: 'location',
+            message:"where do you live?",
             validate:userInfo => {
                 if (userInfo) {
                     return true;
                 }else{
-                    console.log("please enter a valid name!");
+                    console.log("please enter a valid location!");
                     return false;
                 }
             }
-    
+
         },
- 
-    // location
-    {
-        type: 'input',
-        name: 'location',
-        message:"where do you live?",
-        validate:userInfo => {
-            if (userInfo) {
-                return true;
-            }else{
-                console.log("please enter a valid location!");
-                return false;
+
+        // Linked account 
+        {
+            type: 'input',
+            name: 'resume',
+            message:"what's your linkEd account?",
+            validate:userInfo => {
+                if (userInfo) {
+                    return true;
+                }else{
+                    console.log("please enter a valid account!");
+                    return false;
+                }
             }
-        }
 
-    },
-
-     // Linked account 
-     {
-        type: 'input',
-        name: 'resume',
-        message:"what's your linkEd account?",
-        validate:userInfo => {
-            if (userInfo) {
-                return true;
-            }else{
-                console.log("please enter a valid account!");
-                return false;
-            }
-        }
-
-    },
+        },
 
         // github account
     {
@@ -83,7 +83,8 @@ const questions = [
         }
 
     },
-         // new project name 
+            
+            // new project name 
          {
             type: 'input',
             name: 'nameProject',
@@ -99,7 +100,7 @@ const questions = [
     
         },
 
-        //  project description 
+            //  project description (input)
         {
             type: 'input',
             name: 'describePro',
@@ -114,19 +115,115 @@ const questions = [
             }
     
         },
-        // license and registration
+
+              // project link to live 
+        {
+            type: 'confirm',
+            name: 'confirmLive',
+            message: 'Would you like to enter enter a link to the live site, if it exists?',
+            default: false,
+        },
+
+        {
+            type: 'input',
+            name: 'liveSite',
+            message: 'Provide a full link (include "https://") to the live site.',
+            when: ({ confirmLiveLInk }) => confirmLiveLInk,
+            validate: liveSiteLink => {
+                if (liveSiteLink) {
+                return true;
+                } else {
+                console.log('You need to enter a link to the live site!');
+                return false;
+                }
+        }
+        },
+
+
+        //     // project link walk through
+        {
+            type: 'confirm',
+            name: 'confirmDemo',
+            message: 'Would you like to embed a video or gif walkthrough or demo, if it exists?',
+            default: false,
+        },
+        {
+            type: 'input',
+            name: 'siteDemo',
+            message: 'Provide a link to embed the gif or video.',
+            when: ({ confirmDemo }) => confirmDemo,
+            validate: siteDemo => {
+                if (siteDemo) {
+                return true;
+                } else {
+                console.log('You need to enter a link to embed a video or gif walkthrough or demo!');
+                return false;
+                }
+        }
+        },  
+
+
+        // license and registration please (input)
         {
             type:'list',
-            name:'license',
+            name:'licenseChoice',
             message:'What license is needed for project?',
             choices: [
                 'MIT',
                 'Apache 2.0',
                 'GPL 3.0',
                 'None'
-            ],
+            ]
             
+        },
+
+
+        // project installation
+    {
+        type: 'input',
+        name: 'installationIns',
+        message: 'Provide instructions to install your project.',
+        validate: installationIns => {
+            if (installationIns) {
+            return true;
+            } else {
+            console.log('Please provide instructions to install your project!');
+            return false;
+            }
+    }
+    },  
+
+        // project usage
+    {
+        type: 'input',
+        name: 'usageInstructions',
+        message: 'Provide instructions to use your project.',
+        validate: usageInstructions => {
+            if (usageInstructions) {
+            return true;
+            } else {
+            console.log('Please provide instructions to use your project!');
+            return false;
+            }
         }
+    },  
+         
+    
+        // project test
+    {
+        type: 'input',
+        name: 'testInstructions',
+        message: 'Provide instructions on how users can test your project.',
+        validate: testInstructions => {
+            if (testInstructions) {
+            return true;
+            } else {
+            console.log('You need to provide instructions on how users can test your project!');
+            return false;
+            }
+        }
+    },
+
     
 ];
 
@@ -141,12 +238,22 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
+
+    console.log(`
+    =================
+    Welcome to the ReadMe Generator! 
+    Answer the following question to create a ReadMe file.
+    =================
+    `);
+
+
+
     inquirer.prompt(questions)
     .then((data) => {
-        // console.log(data);
+        console.log(data);
         // const template = generateMarkdown(data)
         // console.log(template);
-        writeToFile("./dist/readme.md", generateMarkdown())
+        writeToFile("./dist/readme.md", generateMarkdown(data))
     })
 
 }
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
+function renderLicenseBadge(license) {
+  let badgeLabel = license.replace(" ", "&ensp;");
+  return `
+  [![Generic badge](https://img.shields.io/badge/License-${badgeLabel}-green.svg)](${renderLicenseLink(license)})
+  `
+};
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let linkUrl = license.toLowerCase().replace(" ", "-");
+  return `https://choosealicense.com/licenses/${linkUrl}/.`
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  if (license === "None") {
+    return "";
+  } else {
+
+    return `
+  ### Licensing 
+  ${license}
+  ${renderLicenseBadge(license)}
+  `
+}
+};
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  const {}
-  return `# ${data.title}
+  // console.log(data)
+  const { github, location, resume, whatEmail, nameProject, licenseChoice, liveLink, liveSite, siteDemoLink, ...info } = data;
 
-`;
+  return `
+  # ${data.projectTitle}
+
+  ## Table of Contents
+  - [Description](#project-description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
+
+
+  ## Project Description
+
+  ## Installation 
+  ${data.installationIns}
+
+  ## Usage 
+  ${data.usageInstructions}
+
+  ## Testing
+  ${data.testInstructions}
+
+  ## Questions
+  Reach out to the repo owner, [${github}](https://github.com/${github}) at ${data.whatEmail}.
+  `;
 }
 
 module.exports = generateMarkdown;
